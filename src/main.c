068333a675994529cfd4a890f283ff82d4247645
MarkTBSS <marktbss@gmail.com>
2023-11-12 10:43:52 +0700

    [MOD] Ternary Added

--- src/main.c
+++ src/main.c
@@ -9,12 +9,20 @@ int max_of_four(int a, int b, int c, int d) {
     return max;
 }
 
+int max_of_four_ternary(int a, int b, int c, int d) {
+    int max = (a > b) ? ((a > c) ? ((a > d) ? a : d) : c) : ((b > c) ? ((b > d) ? b : d) : c);
+    return max;
+}
+
 int main() {
     int a = 3;
     int b = 4;
     int c = 6;
     int d = 5;
     int result = max_of_four(a, b, c, d);
-    printf("The biggest value is: %d", result);
+    printf("The biggest value is: %d\n", result);
+
+    int result_ternary = max_of_four_ternary(a, b, c, d);
+    printf("The biggest value is: %d", result_ternary);
     return 0;
 }
